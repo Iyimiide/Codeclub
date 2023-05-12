2c930f14301fef240496c78c741a79498de4f2f2
Iyimiide <124675039+Iyimiide@users.noreply.github.com>
2023-05-12 14:42:35 +0100

    Update About.php

--- About.php
+++ About.php
@@ -1,7 +1,7 @@
 <<html lang="en">
   <head>
     <meta charset="UTF-8">
-    <title>LEASE WITH EASE</title>
+    <title>Coding with Tim</title>
           <style>
 /* CSS for logo */
 #logo {
@@ -74,23 +74,20 @@ footer {
      </head>
     <body>
       <header>
-        <img src="Assets/LeaseWithEase.png" alt="logo" id="logo">
+        <img src="Assets/Logo.png" alt="logo" id="logo">
         <nav>
               <ul>
                   <li> <a href="index.html">Home</a></li>
                   <li> <a href="About.php">About Us</a></li>
-                  <li> <a href="listings.php">Listings</a></li>
-                  <li> <a href="login.php">Realtors Sign Up/Login</a></li>
-                  <li> <a href="login2.php">Admin</a></li>
+                  <li> <a href="Activities.php">Activities</a></li>
+                  <li> <a href="login.php">Sign Up/Login</a></li>
               </ul>
           </nav>
           </header>
     <main>
     <section>
       <h1>About Us</h1>
-      <p>At Lease With Ease, we understand that the process of leasing land can be a complicated and time-consuming process. That's why we've made it our mission to help individuals and businesses navigate the process with ease.
-Our team of experienced professionals has a deep understanding of the local real estate market and the legal requirements involved in leasing land. We work closely with our clients to understand their unique needs and provide tailored solutions to meet their specific requirements.
-Whether you're looking to lease land for commercial, residential, or agricultural purposes,Lease With Ease can help. We are committed to providing transparent, efficient, and cost-effective services that simplify the process and save our clients time and money.</p>
+      <p>Type in things about us here</p>
       <p>Since our founding in 2023, we have grown to become a national leader in the Real Estate industry. We have achieved this success through our commitment to quality, innovation, and customer service.</p>
       <p>Our team of dedicated professionals includes Realtors, lawyers, designers, engineers, and customer service representatives who work together to ensure that our customers receive the best possible experience when using our service.</p>
     </section>
